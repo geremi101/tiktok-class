f3283093bd04f3bc48e3d327f09a2fd4a6e1a776
Eze Geremi Matthew <geremilibrary@gmail.com>
2025-03-19 00:53:13 +0100

    Added lex class to it

---
+++ lex/index.php
@@ -0,0 +1,137 @@
+<?php
+	
+	# this is a single line comment too
+	// this is a sigle line comment 
+	/*
+		this is a multi line comment 
+	
+		String 
+		Data Type 
+		operator 
+		functions 
+		solve some probelm
+	
+
+	// String 
+
+		$g = "<b>Geremi</b> <br />";
+
+		echo '$g is the man that is MAD! <br />';
+
+	// str lenght 
+		echo strlen("GEREMI tha man that is MAD");
+
+	// world count 
+		str_word_count("Geremi the man that is MAD");
+	// Search for a string 
+		strpos("Geremi tha man that is MAD", "Geremi");
+
+	// Convert the upper case
+
+		echo strtoupper($g);
+	// Convert to lower 
+		echo strtolower($g);
+	$txt = "there is a man up there";
+	echo str_replace("man", "Woman", $txt);
+	echo "<br />";
+	echo strrev($txt);
+
+	// Remove white space 
+	trim($g);
+
+	// String Concatenation
+
+	$txt1 = " this is another string";
+
+	$newText = $txt . $txt1;
+
+	echo $newText;
+
+
+	echo "$txt $txt1";
+
+	// PHP Super Globals 
+
+	/*
+		09164324075
+
+		1. $_SERVER
+		2. $_SESSION
+		3. $_REQUEST
+		4. $_POST
+		5. $_GET
+		6. $_FILES
+		7. $_GLOBAL
+		8. $_ENV
+		9. $_COOKIES
+
+		The are pre-defined Var
+		accessible regardless of scope
+	*/
+
+
+?>
+
+
+<?php
+	// 09164324075
+	/*
+	// Return the filename of the currenly executing script
+	echo $_SERVER['PHP_SELF'];
+	echo '<br />';
+	echo $_SERVER['SERVER_NAME'];
+
+	echo '<br />';
+
+	echo $_SERVER['SERVER_ADDR'];
+	echo '<br />';
+	echo $_SERVER['REQUEST_METHOD'];
+	*/
+?>
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>E - Commerce</title>
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+		
+	</head>
+	<body>
+		<div class="container-fluid">
+			<h1>Welcome to our site</h1>
+
+			<div class="card">
+				<div class="card-header">
+				    Edit Profile
+				</div>
+			  	<div class="card-body">
+			  		<?php
+
+			  	echo isset($_GET["q"]) ? 
+			  		'<div class="alert alert-danger">'.$_GET["q"].'</div>' : "";
+
+			  	echo isset($_GET["s"]) ? 
+			  		'<div class="alert alert-success">'.$_GET["s"].'</div>' : "";
+					?>
+	<form method="post" action="submit.php" enctype="multipart/form-data">
+		<div class="mb-3">
+		  <label for="pix" class="form-label">	Upload Your Profile Pix
+		  </label>
+		  <input class="form-control" type="file" id="pix" name="pix" required />
+		</div>
+
+					  <button type="submit" class="btn btn-success">Upload</button>
+					</form>
+			 	</div>
+			</div>
+
+		</div>
+
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+	</body>
+</html>
---
+++ lex/submit.php
@@ -0,0 +1,147 @@
+<?php
+	/*
+	if($_SERVER['REQUEST_METHOD'] !== 'POST') {
+		header("LOCATION: index.php");
+		exit();
+	} 
+
+	if(!isset($_POST['email'], $_POST['passdfword']) ){
+		header("LOCATION: index.php?q=Fill the login Form");
+		exit();
+	}
+
+	*/
+
+	// $_REQUEST
+	// $_POST 
+	// $_GET
+	/*
+	$email = $_REQUEST['email'];
+	$password = $_REQUEST['password'];
+
+	$email = trim($email);
+	$email = strtolower( $email );
+
+	if ($email == "geremi@geremi.com" && $password === "geremi")
+		echo "Login Successfull";
+	else
+		header("LOCATION: index.php?q=wrong login details");	
+
+	*/
+
+	/*
+		buildv a simple company website with minimum of 4 pages 
+		create a contact form and process the form
+
+	*/
+
+	// var dump
+	// var_dump($_SERVER);
+
+	/*
+		ASSIGNMENT:
+
+		Create a Registration Form
+		the field include 
+			Name
+			Email
+			Phone
+			username
+			password
+			confirm password
+			Gender
+			Country 
+
+	*/
+
+/*
+	algorithm 
+	1. take my bath
+	2. wake up from sleep 
+	3. eat break on dining at home 
+	4. get to the office 
+	5. take off my night gown 
+	6. greet my boss
+	7. attend to client
+	8. wear my office cloth 
+	9. do excecise
+	10. take the bus going to my officey
+
+
+	1. wake up from sleep
+	2. do excecise
+	3. take off the night gown
+	4. take my bath
+	5. wear office cloth
+	6. eat break fast on dining talbe
+	7. take a bus going to the office
+	8. get to the office
+	9. greet my boss
+	10. attend to client 
+
+	-> algorithm ambiguous 
+	-> it start and end 
+	-> 
+	
+	upload files to server
+
+
+	1. check if the form is submit
+	2. get the form
+	3. check file type
+	3. check the size
+	4. update the file that i upload 
+	5. 
+
+	1. check if the form is submited
+	2. check if there is an error with the file 
+	3. check the size 
+	3. chech the file type
+	4. save the file
+
+	Assignment
+	 Correct the logical error on this code.
+	 the uploaded image will replace the already
+	 stored image in the upload folder
+	 if the name if the same.
+
+*/
+	
+
+	// Check if the form is submited 
+	if($_SERVER['REQUEST_METHOD'] !== 'POST') {
+		header("LOCATION: index.php");
+		exit();
+	} 
+
+	// check if there is an error with the file
+	if($_FILES['pix']['error'] > 0){
+
+		header("LOCATION: index.php?q=error with the file you uploaded!");
+		exit();
+	}
+
+	// check file size
+	if($_FILES['pix']['size'] > 500000){
+		header("LOCATION: index.php?q=File too large!");
+		exit();
+	}
+
+	// check if the file is an image
+	if(getimagesize($_FILES['pix']['tmp_name']) === false){
+		header("LOCATION: index.php?q=File too large!");
+		exit();
+	}
+
+
+	if ( move_uploaded_file($_FILES['pix']['tmp_name'], "upload/".$_FILES['pix']['name']) ){
+		header("LOCATION: index.php?s=File uploaded succefully!");
+		exit();
+	}else{
+		header("LOCATION: index.php?q=There was an error uploading file");
+		exit();
+	}
+
+
+
+
